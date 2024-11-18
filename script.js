7d25df022d0f84beab9acb0ae12f3f90aa181526
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2024-11-18 04:14:51 +0000

    estruturação final

--- script.js
+++ script.js
@@ -13,36 +13,36 @@ let typingEffect = new Typed(".typedText", {
     backDelay: 2000,
 })
 
-const sr = ScrollReveal({
-    origin: "top",
-    distance: "80px",
-    duration: "2000",
-    reset: "true",
-});
+//const sr = ScrollReveal({
+    //origin: "top",
+    //distance: "80px",
+    //duration: "2000",
+    //reset: "true",
+//});
 
-sr.reveal(".featured-name", {delay: 100});
-sr.reveal(".text-info", {delay: 200});
-sr.reveal(".text-btn", {delay: 200});
-sr.reveal(".social-icons", {delay: 200});
-sr.reveal(".featured-image", {delay: 320});
+//sr.reveal(".featured-name", {delay: 100});
+//sr.reveal(".text-info", {delay: 200});
+//sr.reveal(".text-btn", {delay: 200});
+//sr.reveal(".social-icons", {delay: 200});
+//sr.reveal(".featured-image", {delay: 320});
 
-sr.reveal(".project.box", {interval: 200});
+//sr.reveal(".project.box", {interval: 200});
 
-sr.reveal(".top-header",{});
+//sr.reveal(".top-header",{});
 
-const srLeft = ScrollReveal({
-    origin: "left",
-    distance: "80px",
-    duration: 2000,
-    reset: true,
-});
+//const srLeft = ScrollReveal({
+   //origin: "left",
+    //distance: "80px",
+    //duration: 2000,
+    //reset: true,
+//});
 
 
-const srRight = ScrollReveal({
-    origin: "left",
-    distance: "80px",
-    duration: 2000,
-    reset: true,
-});
+//const srRight = ScrollReveal({
+    //origin: "left",
+    //distance: "80px",
+    //duration: 2000,
+    //reset: true,
+//});
 
 
