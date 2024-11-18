db1ef66c3153f1b9b54a4861da8be0fe725412d4
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2024-11-18 02:23:33 +0000

    ajustes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function myMenuFunction() {
-    let menuBtn = document.getElementById("myNavMenu");
+function openMenu() {
+    // Adiciona a classe 'responsive' ao menu para abrir
+    document.getElementById('myNavMenu').classList.add('responsive');
 
-    if (menuBtn.className === "nav-menu") {
-        menuBtn.className += "responsive";
-    } else {
-        menuBtn.className = "nav-menu";
-   }
+    // Atualiza os botões de abrir e fechar o menu
+    document.getElementById('openMenu').classList.add('hidden'); // Esconde o botão abrir
+    document.getElementById('closeMenu').classList.remove('hidden'); // Mostra o botão fechar
 }
+
+function closeMenu() {
+    // Remove a classe 'responsive' do menu para fechar
+    document.getElementById('myNavMenu').classList.remove('responsive');
+
+    // Atualiza os botões de abrir e fechar o menu
+    document.getElementById('openMenu').classList.remove('hidden'); // Mostra o botão abrir
+    document.getElementById('closeMenu').classList.add('hidden'); // Esconde o botão fechar
+}
+
 
 const body = document.querySelector("body"),
     toggleSwich = document.getElementById("toggle-switch");
