6e584ee2298c097747edfd4dfa6e9cfac9ea41e7
MinYonhee <163359267+MinYonhee@users.noreply.github.com>
2024-11-18 03:41:35 +0000

    ajustes

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-function openMenu() {
-    // Adiciona a classe 'responsive' ao menu para abrir
-    document.getElementById('myNavMenu').classList.add('responsive');
-
-    // Atualiza os botões de abrir e fechar o menu
-    document.getElementById('openMenu').classList.add('hidden'); // Esconde o botão abrir
-    document.getElementById('closeMenu').classList.remove('hidden'); // Mostra o botão fechar
-}
-
-function closeMenu() {
-    // Remove a classe 'responsive' do menu para fechar
-    document.getElementById('myNavMenu').classList.remove('responsive');
-
-    // Atualiza os botões de abrir e fechar o menu
-    document.getElementById('openMenu').classList.remove('hidden'); // Mostra o botão abrir
-    document.getElementById('closeMenu').classList.add('hidden'); // Esconde o botão fechar
-}
-
-
 const body = document.querySelector("body"),
     toggleSwich = document.getElementById("toggle-switch");
 toggleSwich.addEventListener("click", ()=> {
@@ -31,3 +12,37 @@ let typingEffect = new Typed(".typedText", {
     backSpeed: 80,
     backDelay: 2000,
 })
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: "2000",
+    reset: "true",
+});
+
+sr.reveal(".featured-name", {delay: 100});
+sr.reveal(".text-info", {delay: 200});
+sr.reveal(".text-btn", {delay: 200});
+sr.reveal(".social-icons", {delay: 200});
+sr.reveal(".featured-image", {delay: 320});
+
+sr.reveal(".project.box", {interval: 200});
+
+sr.reveal(".top-header",{});
+
+const srLeft = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+
+
+const srRight = ScrollReveal({
+    origin: "left",
+    distance: "80px",
+    duration: 2000,
+    reset: true,
+});
+
+
